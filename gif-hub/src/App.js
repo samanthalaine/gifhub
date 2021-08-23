cf7670179ab0b7c6619bec33dd314bd9e284f7cd
andres <andres.gordillo@mail.citytech.cuny.edu>
2021-08-23 12:25:56 -0400

    Fetched data

--- gif-hub/src/App.js
+++ gif-hub/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react"
 
 function App() {
+
+  const [gif, setGif] = useState([])
+  useEffect(() => {
+    fetch("https://api.giphy.com/v1/gifs/random?api_key=xjRmS04E6Syed3yZKqVZZ301OmqO5rvL&tag=&rating=g")
+    .then((res) => res.json())
+    .then((data) => setGif(data))
+
+  }, [])
+
+console.log(gif)
+ const imageUrl= gif.data.image_url
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={imageUrl}></img>
     </div>
   );
 }
